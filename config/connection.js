521c25bfe8392aa5a71843f008d11f56c1d14740
wollenet <ewollenberg16@jcu.edu>
2018-02-07 13:41:08 -0500

    burger homework

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	})
+};
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+
+module.exports = connection;
